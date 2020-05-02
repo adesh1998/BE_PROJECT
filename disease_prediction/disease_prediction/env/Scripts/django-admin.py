446af453aaf4e978f514a20acabe9ac9030f11f8
adesh1998 <aadesh.15298@gmail.com>
2020-05-02 13:10:42 +0530

    02/05/2020

---
+++ disease_prediction/disease_prediction/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\adesh\source\repos\disease_prediction\disease_prediction\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
